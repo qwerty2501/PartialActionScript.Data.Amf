25e2d32b645a3480c2828756f8b3ed6601ed9fc9
クワーティ <riot313@gmail.com>
2013-11-30 00:33:33 +0900

    create resource file

--- PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/ExceptionHelper.cs
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/ExceptionHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
 
 namespace PartialActionScript.Data.Amf
 {
@@ -10,29 +11,41 @@ namespace PartialActionScript.Data.Amf
     {
         internal static InvalidOperationException CreateInvalidTypeException(AmfValueType actualType)
         {
-            return new InvalidOperationException("Bud amf type: " + actualType);
+            
+            return new InvalidOperationException(string.Format(getloaderString("InvalidTypeErrorMessageFormat"), actualType));
         }
 
         internal static InvalidOperationException CreateInvalidRemainingValueException(UInt29 val)
         {
-            return new InvalidOperationException("Invalid remaining value:" +  val);
+            return new InvalidOperationException(string.Format(getloaderString("InvalidRemainingValueErrorMessageFormat"),  val));
         }
 
         internal static InvalidOperationException CreateInvalidOperationStringValueTooLong(string val)
         {
-            return new InvalidOperationException("string length too long:" + val.Length);
+
+            return new InvalidOperationException(string.Format(getloaderString("StringValueTooLongErrorMessageFormat") , val.Length));
         }
 
         internal static OverflowException CreateOutOfUInt29Exception(UInt32 input)
         {
-            return new OverflowException("Over  UInt29 :" + input);
+            return new OverflowException(string.Format(getloaderString("OutOfUInt29ErrorMessageFormat"), input));
         }
 
         internal static OverflowException CreateOutOfInt29Exception(Int32 input)
         {
-            return new OverflowException("Over Int29:" + input);
+            return new OverflowException(string.Format(getloaderString("OutOfInt29ErrorMessageFormat"), input));
         }
 
+
+        private static string getloaderString(string resource)
+        {
+#if NETFX_CORE
+            var resourceLoarder = new ResourceLoader();
+            return resourceLoarder.GetString(resource);
+#else
+            return resource;
+#endif
+        }
 
     }
 }

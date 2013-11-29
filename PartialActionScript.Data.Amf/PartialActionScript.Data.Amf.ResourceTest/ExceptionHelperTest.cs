c8744015a09fd1adee813b06e6fe6026a859e3b3
クワーティ <riot313@gmail.com>
2013-11-30 00:35:36 +0900

    create Resource test

---
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf.ResourceTest/ExceptionHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace PartialActionScript.Data.Amf.ResourceTest
+{
+    [TestClass]
+    public class ExceptionHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

58ebd6cfc1fc034609f3bd13b0b77e9f3ee4f1fc
クワーティ <riot313@gmail.com>
2013-12-04 23:23:18 +0900

    modified xml context value

---
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/XmlContextValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+
+namespace PartialActionScript.Data.Amf
+{
+    internal struct XmlContextValue
+    {
+        internal XmlContextValue(XmlDocument document, bool newer)
+        {
+            this.Document = document;
+            this.Newer = newer;
+        }
+
+        internal XmlDocument Document { get; private set; }
+
+        internal bool Newer { get; private set; }
+    }
+}

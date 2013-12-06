2410025e0e05bb6df0f7b73a476a3dd8e25ae8a8
クワーティ <riot313@gmail.com>
2013-12-06 21:17:25 +0900

    created DateTimeOffset extention

--- PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/DateTimeOffsetExtention.cs
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/DateTimeOffsetExtention.cs
@@ -10,15 +10,15 @@ namespace PartialActionScript.Data.Amf
     {
         internal static double ToUnixTime(this DateTimeOffset self)
         {
-            return (self - unixEpoch_).TotalMilliseconds;
+            return (self - unixEpoch).TotalMilliseconds;
         }
 
         internal static DateTimeOffset FromUnixTime(double unixTime)
         {
-            return unixEpoch_.Add(TimeSpan.FromMilliseconds(unixTime));
+            return unixEpoch.AddMilliseconds(unixTime);
         }
 
 
-        private static readonly DateTimeOffset unixEpoch_ = new DateTimeOffset(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+        private static readonly DateTimeOffset unixEpoch = new DateTimeOffset(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
     }
 }

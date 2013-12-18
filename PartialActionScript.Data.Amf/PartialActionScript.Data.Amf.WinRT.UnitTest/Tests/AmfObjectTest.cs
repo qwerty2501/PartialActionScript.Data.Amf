67c6a63888c534e1c70db57fa1c3b9a126aa39c6
クワーティ <riot313@gmail.com>
2013-12-18 22:01:04 +0900

    added AmfObject test

--- PartialActionScript.Data.Amf/PartialActionScript.Data.Amf.WinRT.UnitTest/Tests/AmfObjectTest.cs
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf.WinRT.UnitTest/Tests/AmfObjectTest.cs
@@ -1,1 +1,207 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Storage.Streams;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.IO;
+
+namespace PartialActionScript.Data.Amf.UnitTest
+{
+    [TestClass]
+    public class AmfObjectValueTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var val = createGeneralAmfValue();
+            Assert.AreEqual(AmfValueType.Object, val.ValueType);
+
+
+        }
+
+        [TestMethod]
+        public void GetStringTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetString();
+            });
+        }
+
+        [TestMethod]
+
+        public void GetBooleanTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetBoolean();
+            });
+
+        }
+
+        [TestMethod]
+        public void GetArrayTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetArray();
+            });
+
+        }
+
+        [TestMethod]
+        public void GetDateTest()
+        {
+            var val = createGeneralAmfValue();
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetDate();
+            });
+
+        }
+
+        [TestMethod]
+        public void GetXmlTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetXml();
+            });
+        }
+
+        [TestMethod]
+        public void GetByteArrayTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetByteArray();
+            });
+
+        }
+
+        [TestMethod]
+        public void GetNumberTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetNumber();
+            });
+        }
+
+        [TestMethod]
+        public void GetObjectTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.AreEqual(val, val.GetObject());
+        }
+
+        [TestMethod]
+        public void GetVectorIntTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetVectorInt();
+            });
+        }
+
+        [TestMethod]
+        public void GetVectorUIntTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetVectorUInt();
+            });
+        }
+
+        [TestMethod]
+        public void GetVectorDoubleTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetVectorDouble();
+            });
+        }
+
+
+
+        [TestMethod]
+        public void GetVectorObjectTest()
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                val.GetVectorObject();
+            });
+        }
+
+        [DataTestMethod]
+        [DataRow( "test:testvalue")]
+        public void ToStringTest( string expected)
+        {
+            var val = createGeneralAmfValue();
+
+            Assert.AreEqual(expected.ToString(), val.ToString());
+
+        }
+
+        [DataTestMethod]
+        [DataRow("3", "t")]
+        public void Amf3ParseTest(string input, string expected)
+        {
+            var actualArray = TestHelper.CreateByteArray(input);
+
+
+            var actual = AmfValue.Parse(actualArray.AsBuffer(), AmfEncodingType.Amf3);
+
+            Assert.AreEqual(expected, actual.GetObject());
+
+        }
+
+        [DataTestMethod]
+        [DataRow("t", "3")]
+        public void Amf3SequencifyTest(string input, string expect)
+        {
+            var expectArray = TestHelper.CreateByteArray(expect);
+            var actual = createGeneralAmfValue();
+
+
+            var buffer = actual.Sequencify(AmfEncodingType.Amf3);
+
+            CollectionAssert.AreEqual(expectArray, buffer.ToArray());
+
+        }
+
+
+        private AmfObject createGeneralAmfValue()
+        {
+            var obj = new AmfObject();
+
+            obj["test"] = AmfValue.CreateStringValue("testvalue");
+
+            return obj; 
+
+        }
+    }
+}
--- PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/AmfObject.cs
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/AmfObject.cs
@@ -1,30 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Windows.Data.Xml.Dom;
+using Windows.Storage.Streams;
 
 namespace PartialActionScript.Data.Amf
 {
-    public sealed class AmfObject
+    public sealed class AmfObject:IAmfValue,IDictionary<string,IAmfValue>
     {
         #region Constructor
 
-        AmfObject()
+        public AmfObject()
         {
-
+            propertyMap_ = new Dictionary<string, IAmfValue>();
         }
 
 
         #endregion
 
-        #region Finalizer
-
-        ~AmfObject()
-        {
-
-        }
-
-        #endregion
 
         #region Property
 
@@ -33,10 +28,182 @@ namespace PartialActionScript.Data.Amf
 
         #region Method
 
+        public override string ToString()
+        {
+            return string.Join(",", (from property in this.propertyMap_
+                                     select property.Key + ":" + property.Value.ToString()));
+        }
 
         #endregion
 
         #region Private
+
+        private IDictionary<string, IAmfValue> propertyMap_;
+
+        #endregion
+
+        #region IAmfValue
+
+        public AmfValueType ValueType
+        {
+            get { return AmfValueType.Object; }
+        }
+
+        public bool GetBoolean()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public AmfArray GetArray()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public string GetString()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public AmfObject GetObject()
+        {
+            return this;
+        }
+
+        public DateTimeOffset GetDate()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public XmlDocument GetXml()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IReadOnlyList<byte> GetByteArray()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IReadOnlyList<int> GetVectorInt()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IReadOnlyList<uint> GetVectorUInt()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IReadOnlyList<double> GetVectorDouble()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IReadOnlyList<IAmfValue> GetVectorObject()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public double GetNumber()
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        public IBuffer Sequencify(AmfEncodingType encodingType)
+        {
+            throw ExceptionHelper.CreateInvalidTypeException();
+        }
+
+        #endregion
+
+        #region IDictionary
+
+        public void Add(string key, IAmfValue value)
+        {
+            this.propertyMap_.Add(key, value);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return this.propertyMap_.ContainsKey(key);
+        }
+
+        public ICollection<string> Keys
+        {
+            get { return this.propertyMap_.Keys; }
+        }
+
+        public bool Remove(string key)
+        {
+            return this.propertyMap_.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out IAmfValue value)
+        {
+            return this.propertyMap_.TryGetValue(key, out value);
+        }
+
+        public ICollection<IAmfValue> Values
+        {
+            get { return this.propertyMap_.Values; }
+        }
+
+        public IAmfValue this[string key]
+        {
+            get
+            {
+                return this.propertyMap_[key];
+            }
+            set
+            {
+                this.propertyMap_[key] = value;
+            }
+        }
+
+        public void Add(KeyValuePair<string, IAmfValue> item)
+        {
+            this.propertyMap_.Add(item);
+        }
+
+        public void Clear()
+        {
+            this.propertyMap_.Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, IAmfValue> item)
+        {
+            return this.propertyMap_.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, IAmfValue>[] array, int arrayIndex)
+        {
+            this.propertyMap_.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return this.propertyMap_.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return this.propertyMap_.IsReadOnly; }
+        }
+
+        public bool Remove(KeyValuePair<string, IAmfValue> item)
+        {
+            return this.propertyMap_.Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<string, IAmfValue>> GetEnumerator()
+        {
+            return this.propertyMap_.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
 
         #endregion
     }

4a7eb20fa138e6790d52655fc9b0db2623e77d07
クワーティ <riot313@gmail.com>
2013-12-21 21:39:30 +0900

    Amf3ObjectTraitsInfo

---
+++ PartialActionScript.Data.Amf/PartialActionScript.Data.Amf/Amf3ObjectTraitsInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialActionScript.Data.Amf
+{
+    internal struct Amf3ObjectTraitsInfo :IEquatable<Amf3ObjectTraitsInfo>
+    {
+        internal Amf3ObjectTraitsInfo(string typeName, IEnumerable<string> traits)
+        {
+            this.TypeName = typeName;
+            this.Traits = traits;
+        }
+
+        internal string TypeName { get; private set; }
+
+        internal IEnumerable<string> Traits { get; private set; }
+
+
+
+        public bool Equals(Amf3ObjectTraitsInfo other)
+        {
+            return this.TypeName.Equals(other.TypeName) && this.Traits.SequenceEqual(other.Traits);
+        }
+    }
+}
